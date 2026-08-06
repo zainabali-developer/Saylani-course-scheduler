1d1b4b884dc03597df686c4ca67fd55d1251c836
Zainab Ali <codedbyzainab@gmail.com>
2026-08-06 20:41:54 +0500

    fix: client directive position

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -1,5 +1,7 @@
-export const dynamic = "force-dynamic";
+
 "use client";
+
+export const dynamic = "force-dynamic";
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
